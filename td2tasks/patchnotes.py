25ba7b15645c903ed5d15a54d5f1e77523501f79
calebh93 <hawkins.caleb93@gmail.com>
2016-05-18 15:44:27 -0700

    Improved !patchnotes.

--- td2tasks/patchnotes.py
+++ td2tasks/patchnotes.py
@@ -9,9 +9,12 @@ def sanitize(text):
 def run(comment):
     """List recent changes of hero or item based on patch_notes.json."""
 
-    patch_list = ["6.87c", "6.87b", "6.87"]
+    patch_list = ["6.87c", "6.87b", "6.87", "https://nicedotame.me"]
 
     request = comment.body.strip().replace("!patchnotes", "").strip()
+
+    # Addressing the !patchnotes "hero/item" problem
+    request = request.replace("\"", "")
 
     with open("td2tasks/patch_notes.json") as file:
         notes = json.load(file)
@@ -32,9 +35,20 @@ def run(comment):
             return
 
         for heroitem, change in notes[patch_number].iteritems():
-            if sanitize(heroitem).startswith(sanitize(request)):
+            if sanitize(request) in sanitize(heroitem):
                 request = heroitem
                 break
+            else:
+                done = False
+                for word in sanitize(request).split():
+                    if (word in sanitize(heroitem) and word != "the" and
+                            word != "of"):
+                        request = heroitem
+                        done = True
+                        break
+                if done:
+                    break
+
         if request in notes[patch_number]:
             changelog = notes[patch_number][request]
     else:
@@ -43,12 +57,23 @@ def run(comment):
             if done:
                 break
             for heroitem, change in notes[patch].iteritems():
-                if sanitize(heroitem).startswith(sanitize(request)):
+                if sanitize(request) in sanitize(heroitem):
                     patch_number = patch
                     request = heroitem
                     changelog = notes[patch][request]
                     done = True
                     break
+                else:
+                    for word in sanitize(request).split():
+                        if (word in sanitize(heroitem) and word != "the" and
+                                word != "of"):
+                            patch_number = patch
+                            request = heroitem
+                            changelog = notes[patch][request]
+                            done = True
+                            break
+                    if done:
+                        break
 
     if not changelog:
         return
